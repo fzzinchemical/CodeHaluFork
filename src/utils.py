f1a0627ea9ed963b1b3b5f0a07d7a0c6d37c771c
fzzinchemical <mathys162002@gmail.com>
2026-02-23 13:56:12 +0100

    Enhance load_problems function: add UTF-8 encoding for file operations and improve code formatting

--- src/utils.py
+++ src/utils.py
@@ -2,26 +2,29 @@ import os
 import json
 from tqdm import tqdm
 
+
 def load_problems(problems_root, return_dict=False):
+    """Loads problems from the specified root directory."""
     subdirs = [
-        os.path.join(problems_root, d) for d in os.listdir(problems_root) 
-            if os.path.isdir(os.path.join(problems_root, d))
+        os.path.join(problems_root, d) for d in os.listdir(problems_root)
+        if os.path.isdir(os.path.join(problems_root, d))
     ]
     problems = []
-    
+
     for subdir in tqdm(sorted(subdirs)):  # Maintain the same order
         data_json_path = os.path.join(subdir, 'data.json')
         images_folder = os.path.join(subdir, 'images')
         problem_id = os.path.basename(subdir)
 
         # Read data.json
-        with open(data_json_path, 'r') as file:
+        with open(data_json_path, 'r', encoding='utf-8') as file:
             problem_data = json.load(file)
 
             # Load images from 'images' folder
             images = []
-            image_files = sorted([f for f in os.listdir(images_folder) if f.endswith('.png')],
-                                    key=lambda x: int(os.path.splitext(x)[0]))
+            image_files = sorted(
+                [f for f in os.listdir(images_folder) if f.endswith('.png')],
+                key=lambda x: int(os.path.splitext(x)[0]))
             for img_file in image_files:
                 img_path = os.path.join(images_folder, img_file)
                 # image = Image.open(img_path).convert("RGBA")
@@ -32,15 +35,13 @@ def load_problems(problems_root, return_dict=False):
                 # images.append(new_image)
                 image = Image.open(img_path).convert('RGB')
                 images.append(image)
-            
-            problems.append( 
-                {
-                    'problem_id': problem_id,
-                    'problem': problem_data,
-                    'images': images,
-                }
-            )
-    
+
+            problems.append({
+                'problem_id': problem_id,
+                'problem': problem_data,
+                'images': images,
+            })
+
     if return_dict:
         return {p['problem_id']: p for p in problems}
     else:
